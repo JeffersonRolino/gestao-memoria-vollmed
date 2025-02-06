b45d55bd4e5c56a471fb52d5ff5f3c860295c770
jeffersonrolino <jefferson.rolino@gmail.com>
2025-02-06 18:33:24 -0300

    Capítulo 04 - Aula 04: Organizando os dados da execução

---
+++ src/main/java/med/voll/api/testes/memoria/TestaPC.java
@@ -0,0 +1,12 @@
+package med.voll.api.testes.memoria;
+
+public class TestaPC {
+    public static void main(String[] args) {
+        System.out.println(soma(3, 5));
+        System.out.println(soma(4, 9));
+    }
+
+    public static int soma(int valo1, int valor2){
+        return valo1 + valor2;
+    }
+}
