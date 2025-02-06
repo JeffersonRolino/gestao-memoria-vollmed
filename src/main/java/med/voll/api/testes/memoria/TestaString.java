71613ff70c4e4703e1c6a8f3bf47c8b3fd027baf
jeffersonrolino <jefferson.rolino@gmail.com>
2025-02-06 16:46:08 -0300

    Capítulo 02 - Aula 03: Concatenando Strings de forma eficiente

--- src/main/java/med/voll/api/testes/memoria/TestaString.java
+++ src/main/java/med/voll/api/testes/memoria/TestaString.java
@@ -3,11 +3,19 @@ package med.voll.api.testes.memoria;
 public class TestaString {
     public static void main(String[] args) {
         String valor1 = "alura";
-        String valor2 = "alura";
-        System.out.println(valor1 == valor2);
+//        String valor2 = "alura";
+//        System.out.println(valor1 == valor2);
+//
+//        String valor3 = new String("alura");
+//        System.out.println(valor1 == valor3);
 
-        String valor3 = new String("alura");
+        valor1 = valor1 + "Programação";
+        System.out.println(valor1);
 
-        System.out.println(valor1 == valor3);
+        StringBuilder resultado = new StringBuilder();
+        for (int i = 0; i < 50; i++) {
+            resultado.append(i).append(" ");
+        }
+        System.out.println(resultado);
     }
 }
