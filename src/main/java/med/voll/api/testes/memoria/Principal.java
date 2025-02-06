6c07306937178ca7a0390380836527524d0f4963
jeffersonrolino <jefferson.rolino@gmail.com>
2025-02-06 12:41:11 -0300

    Capítulo 01 - Aula 07: Armazenando objetos na Heap

--- src/main/java/med/voll/api/testes/memoria/Principal.java
+++ src/main/java/med/voll/api/testes/memoria/Principal.java
@@ -5,6 +5,11 @@ import med.voll.api.domain.paciente.Paciente;
 public class Principal {
     public static void main(String[] args) {
         Paciente paciente = new Paciente();
-        paciente.consultas();
+        System.out.println(paciente);
+
+        Paciente paciente2 = paciente;
+        System.out.println(paciente2);
+
+        System.out.println(paciente == paciente2);
     }
 }
